a0a6534868f06edd2cc988b82e3f85fadba0be2c
Ben Watson <ben.watson.developer@gmail.com>
2019-02-14 11:28:08 -0500

    updates basic auth to use the Authorization header

--- src/alks.js
+++ src/alks.js
@@ -414,6 +414,10 @@ class alks {
 
     if (opts.userid || opts.password) {
       console.error('The userid and password properties are deprecated and should be replaced with an access token')
+      const credentials = this._base64Encode(`${opts.userid}:${opts.password}`)
+      headers['Authorization'] = `Bearer ${credentials}`
+      delete opts.userid
+      delete opts.password
     }
 
     var responsePromise = opts._fetch(`${opts.baseUrl}/${path}/`, {
