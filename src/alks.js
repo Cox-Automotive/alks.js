a953152d85f8e90b667d6dab3f095c3d5f3f008b
Nick Tang <nick.tang@coxautoinc.com>
2019-10-28 13:40:29 -0400

    return array instead of object for user access endpoints

--- src/alks.js
+++ src/alks.js
@@ -500,7 +500,7 @@ class alks {
   getUserAccess(props) {
     const {accountId} = props
     return(this._doFetch(`userAccess/${accountId}`, props, 'GET').then((results) =>
-      pick(results,['users']))
+      results.users)
     )
   }
 
@@ -526,7 +526,7 @@ class alks {
   getUserRoleAccess(props) {
     const {accountId} = props
     return(this._doFetch(`userAccess/roles/${accountId}`, props).then((results) =>
-      pick(results,['roles']))
+      results.roles)
     )
   }
 
