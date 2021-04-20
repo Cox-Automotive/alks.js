7b54be5242d2b4b3cc46bb03c6ee95ab113f987e
Ben Watson <ben.watson.developer@gmail.com>
2021-04-20 17:24:10 -0400

    fixes a bug

--- src/alks.js
+++ src/alks.js
@@ -728,7 +728,7 @@ class alks {
    */
   getLoginRole(props) {
     const {accountId, role} = props
-    return this._doFetch(`loginRoles/id/${accountId}/${role}`, null, 'GET').then((results) =>
+    return this._doFetch(`loginRoles/id/${accountId}/${role}`, props, 'GET').then((results) =>
       pick(results.loginRole, ['account', 'role', 'iamKeyActive', 'maxKeyDuration']))
   }
 
